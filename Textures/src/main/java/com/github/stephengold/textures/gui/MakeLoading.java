d81b6f03c41e11bb46470bdc5f50e232c09b2145
Stephen Gold <sgold@sonic.net>
2020-11-18 22:49:46 -0800

    add the MakeLoading app

---
+++ Textures/src/main/java/com/github/stephengold/textures/gui/MakeLoading.java
@@ -0,0 +1,137 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.stephengold.textures.gui;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Heart;
+import jme3utilities.MyString;
+import org.imgscalr.Scalr;
+
+/**
+ * A console application to generate the "loading.png" texture.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class MakeLoading {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * size of the texture map (pixels per side)
+     */
+    final private static int textureSize = 2048;
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger
+            = Logger.getLogger(MakeLoading.class.getName());
+    /**
+     * filesystem path to the asset directory/folder for output
+     */
+    final private static String assetDirPath = "build";
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the MakeLoading application.
+     *
+     * @param arguments array of command-line arguments (not null)
+     */
+    public static void main(String[] arguments) {
+        /*
+         * Mute the chatty loggers found in some imported packages.
+         */
+        Heart.setLoggingLevels(Level.WARNING);
+        /*
+         * Set the logging level for this class and also for writeImage().
+         */
+        //logger.setLevel(Level.INFO);
+        //Logger.getLogger(Heart.class.getName()).setLevel(Level.INFO);
+        /*
+         * Instantiate the application.
+         */
+        MakeLoading application = new MakeLoading();
+        /*
+         * Log the working directory.
+         */
+        String userDir = System.getProperty("user.dir");
+        logger.log(Level.INFO, "working directory is {0}",
+                MyString.quote(userDir));
+        /*
+         * Define colors.
+         */
+        float opacity = 1f;
+        Color salmon = new Color(0.9f, 0.35f, 0.35f, opacity);
+        /*
+         * Generate a color image map.
+         */
+        application.makeLoading(salmon, "loading.png");
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Generate an image map for a loading animation.
+     */
+    private void makeLoading(Color fgColor, String assetPath) {
+        /*
+         * Create a blank, color buffered image for the texture map.
+         */
+        BufferedImage image = new BufferedImage(textureSize, textureSize,
+                BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D graphics = image.createGraphics();
+        /*
+         * Fill the circular arc.
+         */
+        double innerRadius = 0.3;
+        double outerRadius = 0.4;
+        graphics.setColor(fgColor);
+        TexUtils.fillSection(graphics, innerRadius, outerRadius,
+                -Math.PI, 0.5 * Math.PI, textureSize);
+        /*
+         * Downsample the image to the desired final size.
+         */
+        int finalSize = 200;
+        BufferedImage downsampledImage = Scalr.resize(image,
+                Scalr.Method.ULTRA_QUALITY, Scalr.Mode.AUTOMATIC, finalSize,
+                finalSize, Scalr.OP_ANTIALIAS);
+        /*
+         * Write the downsampled image to the asset file.
+         */
+        String filePath = String.format("%s/%s", assetDirPath, assetPath);
+        try {
+            Heart.writeImage(filePath, downsampledImage);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
