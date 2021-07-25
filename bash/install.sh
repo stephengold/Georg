f8409c529c211a633366cb0a7ff8d728ac3aacb3
Stephen Gold <sgold@sonic.net>
2021-07-25 15:14:55 -0700

    install.sh: define more environment variables for a more succinct script

--- bash/install.sh
+++ bash/install.sh
@@ -2,27 +2,31 @@
 
 set -eu
 
-SDIR=/home/sgold/Git/Georg/FuzeCreekTextures/build
-DDIR=/home/sgold/Git/FuzeCreek/FC2D/src/main/resources/Textures/cells
-/bin/cp $SDIR/*Bank*.png $DDIR
+CP=/bin/cp
+GIT=/home/sgold/Git
+GEORG=$GIT/Georg
 
-DDIR=/home/sgold/GitFuzeCreek/FC2D/src/main/resources/Textures
-/bin/cp $SDIR/raft*.png $DDIR
+SDIR=$GEORG/FuzeCreekTextures/build
+DDIR=$GIT/FuzeCreek/FC2D/src/main/resources/Textures/cells
+$CP $SDIR/*Bank*.png $DDIR
+
+DDIR=$GIT/FuzeCreek/FC2D/src/main/resources/Textures
+$CP $SDIR/raft*.png $DDIR
 
 
-SDIR=/home/sgold/Git/Georg/HeartTextures/build
-DDIR=/home/sgold/Git/Heart/HeartLibrary/src/main/resources/Textures/shapes
-/bin/cp $SDIR/*.png $DDIR
+SDIR=$GEORG/HeartTextures/build
+DDIR=$GIT/Heart/HeartLibrary/src/main/resources/Textures/shapes
+$CP $SDIR/*.png $DDIR
 
-SDIR=/home/sgold/Git/Georg/MavTextures/build
-DDIR=/home/sgold/Git/jme-vehicles/MavCommon/src/main/resources/Textures/Georg
-/bin/cp $SDIR/horn-silent.png $SDIR/horn-sound.png \
-        $SDIR/left-triangle.png $SDIR/mute.png $SDIR/pause.png \
-        $SDIR/power-off.png $SDIR/power-on.png \
-        $SDIR/run.png $SDIR/single-step.png $SDIR/sound.png  $DDIR
 
-SDIR=/home/sgold/Git/Georg/MavTextures/build
-DDIR=/home/sgold/Git/jme-vehicles/MavLibrary/src/main/resources/Textures/Georg
-/bin/cp $SDIR/compass.png $SDIR/skidmark.png $SDIR/smoke.png \
-        $SDIR/speedo_bg_2.png $SDIR/speedo_needle_2.png \
-        $SDIR/steering.png $SDIR/tachometer_bg.png  $DDIR
+SDIR=$GEORG/MavTextures/build
+DDIR=$GIT/jme-vehicles/MavCommon/src/main/resources/Textures/Georg
+$CP $SDIR/horn-silent.png $SDIR/horn-sound.png \
+    $SDIR/left-triangle.png $SDIR/mute.png $SDIR/pause.png \
+    $SDIR/power-off.png $SDIR/power-on.png \
+    $SDIR/run.png $SDIR/single-step.png $SDIR/sound.png  $DDIR
+
+DDIR=$GIT/jme-vehicles/MavLibrary/src/main/resources/Textures/Georg
+$CP $SDIR/compass.png $SDIR/skidmark.png $SDIR/smoke.png \
+    $SDIR/speedo_bg_2.png $SDIR/speedo_needle_2.png \
+    $SDIR/steering.png $SDIR/tachometer_bg.png  $DDIR
