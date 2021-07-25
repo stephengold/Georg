b37f98bc2b129a7c5dad4cce6cf913301dd81b34
Stephen Gold <sgold@sonic.net>
2021-07-25 15:17:03 -0700

    install.sh: use globbing for a more succinct script

--- bash/install.sh
+++ bash/install.sh
@@ -21,12 +21,9 @@ $CP $SDIR/*.png $DDIR
 
 SDIR=$GEORG/MavTextures/build
 DDIR=$GIT/jme-vehicles/MavCommon/src/main/resources/Textures/Georg
-$CP $SDIR/horn-silent.png $SDIR/horn-sound.png \
-    $SDIR/left-triangle.png $SDIR/mute.png $SDIR/pause.png \
-    $SDIR/power-off.png $SDIR/power-on.png \
-    $SDIR/run.png $SDIR/single-step.png $SDIR/sound.png  $DDIR
+$CP $SDIR/horn-*.png $SDIR/left-triangle.png $SDIR/mute.png $SDIR/pause.png \
+    $SDIR/power-*.png $SDIR/run.png $SDIR/single-step.png $SDIR/sound.png  $DDIR
 
 DDIR=$GIT/jme-vehicles/MavLibrary/src/main/resources/Textures/Georg
 $CP $SDIR/compass.png $SDIR/skidmark.png $SDIR/smoke.png \
-    $SDIR/speedo_bg_2.png $SDIR/speedo_needle_2.png \
-    $SDIR/steering.png $SDIR/tachometer_bg.png  $DDIR
+    $SDIR/speedo*.png $SDIR/steering.png $SDIR/tachometer_bg.png  $DDIR
